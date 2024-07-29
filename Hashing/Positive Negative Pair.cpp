6228a97afdf92ebfc3fac91c3649cc486e0705db
heyanir <anirbansasmal004@gmail.com>
2024-07-29 09:18:49 +0530

    Update Positive Negative Pair.cpp

--- Hashing/Positive Negative Pair.cpp
+++ Hashing/Positive Negative Pair.cpp
@@ -1,1 +1,25 @@
-//will be updated soon
+class Solution{
+public:
+    vector<int> findPairs(int arr[], int n) {
+        unordered_set<int> seen;
+        vector<int> result;
+        
+        for (int i = 0; i < n; ++i) {
+            int num = arr[i];
+            if (seen.find(-num) != seen.end()) {
+                if (num > 0) {
+                    result.push_back(-num);
+                    result.push_back(num);
+                } else {
+                    result.push_back(num);
+                    result.push_back(-num);
+                }
+            }
+            seen.insert(num);
+        }
+        
+        return result;
+    }
+};
+
+//This is Updated code
